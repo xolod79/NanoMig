8a8541a32616bc54b433d1d970b6d8b2efc85e42
xolod79 <kholodov@gmail.com>
2026-07-13 00:59:38 +0300

    initial support Lattice IceSugar Pro

---
+++ src/lattice/icesugar-pro/programmer.bat
@@ -0,0 +1,6 @@
+rem https://github.com/adamgreig/ecpdap
+ecpdap.exe program impl/nanomig_impl.bit
+rem ecpdap.exe flash erase
+rem ecpdap.exe flash write impl/nanomig_impl.bit
+rem ecpdap.exe flash write --offset 0x400000 diagrom.bin
+rem ecpdap.exe flash write --offset 0x480000 kick13.bin
